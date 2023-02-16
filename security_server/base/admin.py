5aeb16327b90e00326eef3d6abc897ab3e4dcb20
Georgi Kunchev <gvkunchev@gmail.com>
2023-02-16 15:27:12 +0200

    Add database

--- security_server/base/admin.py
+++ security_server/base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Sensor
 
-# Register your models here.
+
+class SensorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Sensor, SensorAdmin)
--- security_server/base/models.py
+++ security_server/base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Sensor(models.Model):
+    """Door sensor representation."""
+
+    class Status(models.TextChoices):
+        """Door sensor state representation."""
+        OPEN = 'Open'
+        CLOSED = 'Closed'
+        
+    location = models.CharField(max_length=100)
+    pin_out = models.IntegerField()
+    pin_in = models.IntegerField()
+    state = models.CharField(max_length=20,
+                             choices=Status.choices,
+                             default=Status.OPEN)
+
