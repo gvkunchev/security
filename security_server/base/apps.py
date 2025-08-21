8e4cafaf4626d44b2c849cce8ff5802ec04cd155
Georgi Kunchev <gvkunchev@gmail.com>
2025-08-21 15:47:05 +0300

    Don't run app start on shell

--- security_server/base/apps.py
+++ security_server/base/apps.py
@@ -9,7 +9,7 @@ class BaseConfig(AppConfig):
 
     def ready(self):
         """Create the singletons that monitor the status."""
-        if not 'makemigrations' in sys.argv and not 'migrate' in sys.argv:
+        if not any([arg in sys.argv for arg in ('migrate', 'makemigrations', 'shell')]):
             from base.sensors import Sensors
             from base.armer import Armer
             # This code is not executed during migrations
