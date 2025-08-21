5e76c14ef0a1fc57ebe9416dcf44b61e8228b5ce
Georgi Kunchev <gvkunchev@gmail.com>
2025-08-21 15:35:35 +0300

    Adjust argv lookup

--- security_server/base/apps.py
+++ security_server/base/apps.py
@@ -9,7 +9,7 @@ class BaseConfig(AppConfig):
 
     def ready(self):
         """Create the singletons that monitor the status."""
-        if not 'manage.py' in sys.argv:
+        if not 'makemigrations' in sys.argv and not 'migrate' in sys.argv:
             from base.sensors import Sensors
             from base.armer import Armer
             # This code is not executed during migrations
