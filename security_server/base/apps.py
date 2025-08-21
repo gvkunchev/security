d9184305d77a5558851f055bc78aa5ef81921a82
Georgi Kunchev <gvkunchev@gmail.com>
2025-08-21 15:26:21 +0300

    Attempt on improving the singletons and DB management

--- security_server/base/apps.py
+++ security_server/base/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class BaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'base'
+
+    def ready(self):
+        """Create the singletons that monitor the status."""
+        # This code is not executed during migrations
+        from base.sensors import Sensors
+        from base.armer import Armer
+        Sensors()
+        Armer()
--- security_server/security_server/urls.py
+++ security_server/security_server/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from base.sensors import Sensors
-from base.armer import Armer
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("api.urls"))
 ]
 
-Sensors()
-Armer()
